a50f3edce78094da4ca472fe36368a1b81ce67c4
sravan8855 <sravan630326@gmail.com>
2025-02-12 13:00:22 +0530

    Initial commit

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Angular';
+  imageUrl:string='';
+  name:string='';
+  age!:number;
+}
